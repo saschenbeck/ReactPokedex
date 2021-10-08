318612b5b1da15f3ebddc92fa767d36d0e53632e
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-10-08 10:52:06 -0500

    Initial Layout Complete, Pokemon Data WIP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Dashboard from './Components/Layout/Dashboard';
 import NavBar from "./Components/Layout/NavBar";
+
+import Pokemon from './components/Pokemon/Pokemon';
+
 function App() {
-  return (
-    <div className="App">
+    return (
+    <Router>
+        <div className="App">
           <NavBar />
-          <div className="container">
-              <Dashboard />
+            <div className="container">
+                <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                        <Route exact path="/pokemon/:pokemonIndex" component={Pokemon}/>
+                </Switch>
           </div>
-    </div>
+        </div>
+     </Router>
   );
 }
 
--- src/Components/Pokemon/PokemonCard.js
+++ src/Components/Pokemon/PokemonCard.js
@@ -6,7 +6,19 @@ const Sprite = styled.img`
     width: 5em;
     height: 5em;
     display: none;
-`
+`;
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 10px 10px rgba(0,0,0,0.22)
+    }
+    ~moz-user-select: none;
+    -website-user-select: none;
+    user-select: none;
+    -o-user-select: none;
+`;
 
 export default class PokemonCard extends Component {
     state = {
@@ -33,7 +45,7 @@ export default class PokemonCard extends Component {
         
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
                     {this.state.imageLoading ? (
                         <img src={spinner} style={{width: '5em', height: '5em'}} className="card-img top rounded mx-auto d-block mt-2"></img>
@@ -64,7 +76,7 @@ export default class PokemonCard extends Component {
                                 .join(" ")}
                         </h6>
                     </div>
-                </div>
+                </Card>
             </div>
         );
     }
